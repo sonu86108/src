4f1a6e3aa14553b6059d1e0f4a42929c799e2b11
sonu86108 <sonu006191@gmail.com>
2019-10-08 20:18:37 +0530

    quiz make added

--- main/java/com/sonu/vocabprogress/ui/activities/PlayQuizActivity.java
+++ main/java/com/sonu/vocabprogress/ui/activities/PlayQuizActivity.java
@@ -14,12 +14,18 @@ import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Button;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public class PlayQuizActivity extends AppCompatActivity
 {
 
 	int quizId,quizNumber=0,totalQuiz=10;
 	List<Word> quizWords;
+	List<String> randomWords;
+	List<RadioButton> radioButtons;
 	QuizWordHelper quizWordHelper;
 	TextView  tvQuestion,tvqQNumber;
 	String text="Select the appropriate option for the word: ";
@@ -48,9 +54,16 @@ public class PlayQuizActivity extends AppCompatActivity
 		rbOption2=findViewById(R.id.id_rb_o2);
 		rbOption3=findViewById(R.id.id_rb_o3);
 		rbOption4=findViewById(R.id.id_rb_o4);
+		radioButtons= new ArrayList<>();
+		radioButtons.add(rbOption1);
+		radioButtons.add(rbOption2);
+		radioButtons.add(rbOption3);
+		radioButtons.add(rbOption4);
 		tvqQNumber=findViewById(R.id.id_tv_qNumber);
 		btnNextQuiz=findViewById(R.id.id_btn_next);
 		quizWordHelper=QuizWordHelper.getInstance(this);
+		randomWords=new ArrayList<>();
+		loadWordsFromAssets();
 		
 	}
 	
@@ -63,9 +76,18 @@ public class PlayQuizActivity extends AppCompatActivity
 		tvqQNumber.setText(String.valueOf(1+quizNumber)+"/"+String.valueOf(totalQuiz));
 		tvQuestion.setText(text+word.getWordName());
 		Random random=new Random();
-		((RadioButton)rgOptions.getChildAt(random.nextInt(5)-1)).
+		int rNum=(random.nextInt(4));
+		radioButtons.get(rNum).
 		setText(word.getWordMeaning());
-		if(quizNumber==9){
+		for(int n=0;n<=(radioButtons.size()-1);n++){
+			if(n==rNum){
+				continue;
+			}else if(randomWords.isEmpty()==false){
+				radioButtons.get(n).setText(
+				randomWords.get(random.nextInt(randomWords.size())));
+			}
+		}
+		if(quizWords.size()==quizNumber+1 ||quizNumber==9){
 			btnNextQuiz.setEnabled(false);
 		}
 		quizNumber++;
@@ -75,6 +97,23 @@ public class PlayQuizActivity extends AppCompatActivity
 		setQuiz();
 	}
 	
+	public void loadWordsFromAssets(){
+		InputStreamReader isr=null;
+		try{
+			isr=new InputStreamReader(
+			getAssets().open("random_words.txt"));
+			BufferedReader br=new BufferedReader(isr);
+			String word=new String();
+			while((word=br.readLine())!=null){
+				randomWords.add(word);
+			}
+			isr.close();
+			
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
 	public List<Word> getQuizWords(){
 	    ArrayList<Word> quizWords=new ArrayList<>();
 		Cursor cursor=quizWordHelper.retrieveData(quizId);
